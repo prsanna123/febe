f7ed6008a06f38a3e35eb0d54fa27666916df3e5
prsanna123 <20bd1a050g@gmail.com>
2021-11-30 18:38:21 +0530

    frontend and backend

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+transaction=[("2020-08-25",70.00,"checking"),
+              ("2020-09-25",170.00,"savings"),
+              ("2020-10-25",150.00,"checking"),
+              ]
+l=[]
+@app.route("/",methods=["GET","POST"])
+def home():
+    if request.method == "POST":
+        print(request.form)
+        transaction.append(
+                (
+                    request.form.get("date"),
+                    float(request.form.get("amount")),
+                    request.form.get("account"),
+                )
+            )
+        return render_template("form.html")
+        
+            
+
+@app.route("/transactions")
+def show_transactions():
+    return render_template("transactions.html",entries=transaction)
+
+
+
+
